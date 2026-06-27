ebac73edced5a64744f566008878854ece944889
Brandon Shin <143565532+brshin@users.noreply.github.com>
2026-06-27 15:58:06 -0700

    feat: addLaunchCard component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import LaunchCard from './components/LaunchCard';
 
 export default function App() {
   const [launches, setLaunches] = useState([]);
@@ -6,33 +7,18 @@ export default function App() {
   useEffect(() => {
     fetch('http://localhost:3000/launches')
       .then((res) => res.json())
-      .then((data) => {
-        console.log("API DATA:", data);
-        setLaunches(data);
-      })
+      .then((data) => setLaunches(data))
       .catch((err) => console.error(err));
   }, []);
 
   return (
     <div>
-      <h1 className="text-4xl font-bold ">
-        Upcoming Launches
-      </h1>
+      <h1 className="text-4xl font-bold ">Upcoming Launches</h1>
       <div>
-        {launches.map((launch, index) => (
-          <div>
-            <h2 className="text-xl font-semibold">{launch.name}</h2>
-            <p className="text-gray-400">Scheduled Launch Time: {new Date(launch.net).toLocaleString("en-US", {
-              month: "short",
-              day: "numeric",
-              year: "numeric",
-              hour: "2-digit",
-              minute: "2-digit",
-            })}</p>
-          </div>
+        {launches.map((launch) => (
+          <LaunchCard key={launch.apiId} launch={launch} />
         ))}
       </div>
     </div>
-
   );
 }
